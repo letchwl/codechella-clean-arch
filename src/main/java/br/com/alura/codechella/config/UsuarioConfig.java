0fa0d62a133f103033432c98d31f370d880222c3
letchwl <raullalvespe1227@outlook.com.br>
2025-08-17 16:41:32 -0300

    Listando Usuarios

--- src/main/java/br/com/alura/codechella/config/UsuarioConfig.java
+++ src/main/java/br/com/alura/codechella/config/UsuarioConfig.java
@@ -2,11 +2,13 @@ package br.com.alura.codechella.config;
 
 import br.com.alura.codechella.application.gateways.RepositorioDeUsuario;
 import br.com.alura.codechella.application.usecases.CriarUsuario;
+import br.com.alura.codechella.application.usecases.ListarUsuarios;
 import br.com.alura.codechella.infra.gateways.RepositorioDeUsuarioJpa;
 import br.com.alura.codechella.infra.gateways.UsuarioEntityMapper;
 import br.com.alura.codechella.infra.persistence.UsuarioRepository;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
 
 @Configuration
 public class UsuarioConfig {
@@ -14,6 +16,11 @@ public class UsuarioConfig {
     @Bean
     CriarUsuario criarUsuario(RepositorioDeUsuario repositorioDeUsuario) {
         return new CriarUsuario(repositorioDeUsuario);
+    }
+
+    @Bean
+    ListarUsuarios listarUsuarios(RepositorioDeUsuario repositorioDeUsuario) {
+        return new ListarUsuarios(repositorioDeUsuario);
     }
 
     @Bean
@@ -25,5 +32,4 @@ public class UsuarioConfig {
     UsuarioEntityMapper retornaMapper() {
         return new UsuarioEntityMapper();
     }
-
 }
--- src/main/java/br/com/alura/codechella/infra/controller/UsuarioController.java
+++ src/main/java/br/com/alura/codechella/infra/controller/UsuarioController.java
@@ -1,20 +1,23 @@
 package br.com.alura.codechella.infra.controller;
 
 import br.com.alura.codechella.application.usecases.CriarUsuario;
+import br.com.alura.codechella.application.usecases.ListarUsuarios;
 import br.com.alura.codechella.domain.entities.usuario.Usuario;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/usuarios")
 public class UsuarioController {
 
     private final CriarUsuario criarUsuario;
+    private final ListarUsuarios listarUsuarios;
 
-    public UsuarioController(CriarUsuario criarUsuario) {
+    public UsuarioController(CriarUsuario criarUsuario, ListarUsuarios listarUsuarios) {
         this.criarUsuario = criarUsuario;
+        this.listarUsuarios = listarUsuarios;
     }
 
     @PostMapping
@@ -24,6 +27,13 @@ public class UsuarioController {
 
         return new UsuarioDto(salvo.getCpf(), salvo.getNome(),
                 salvo.getNascimento(), salvo.getEmail());
+    }
+
+    @GetMapping
+    public List<UsuarioDto> listarUsuarios() {
+        return listarUsuarios.listarTodosUsuarios().stream()
+                .map(u -> new UsuarioDto(u.getCpf(), u.getNome(), u.getNascimento(), u.getEmail()))
+                .collect(Collectors.toList());
     }
 
 }
